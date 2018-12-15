6c07731d0f7d77407061bd3f14c40b4f0a44f406
Maryam Kits <MaryamKits@Givis-MacBook-Pro.local>
2018-12-15 13:24:22 +0400

    appRouter

--- site/src/Components/Headphone.jsx
+++ site/src/Components/Headphone.jsx
@@ -1,18 +1,43 @@
 import React from "react";
-// import Products from "../DB/products.json";
-// import { Headphones } from "./Headphones";
 
-export const Headphone = (props, { match }, params) => {
-  const headphone = props.item.find(function(element) {
-    // return element.model === params.headphoneId;
-    return console.log(element.model);
-    // return console.log(match.params);
-  });
-  //   console.log(params);
+const Headphone = props => {
+  alert("wohoo");
+  let item = props.item.find(item => item.id === props.id);
   return (
     <div>
-      headphones
-      {headphone}
+      {console.log(props, "headphone props")}
+      {console.log(item, "headphone item")}
+      <p>brand: {item.brand}</p>
     </div>
   );
 };
+
+export default Headphone;
+
+// import React, { Component } from "react";
+// import axios from "axios";
+// // import Products from "../DB/products.json";
+// import { Headphones } from "./Headphones";
+
+// export default class Headphone extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { headphones: props };
+//     console.log(this.state.headphones);
+//   }
+//   componentDidMount() {
+//     axios.get("/headphones").then(response => {
+//       this.setState(() => {
+//         return {
+//           headphones: response.data
+//         };
+//       });
+//     });
+//   }
+//   render() {
+//     let mapped = this.state.headphones.map(item => {
+//       return <div>product:{item}</div>;
+//     });
+//     return mapped;
+//   }
+// }
--- site/src/Components/Headphones.jsx
+++ site/src/Components/Headphones.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-import { Headphone } from "./Headphone";
+// import AppRouter from "../router/AppRouter";
 import Products from "../DB/products.json";
+import Headphone from "../Components/Headphone";
 
-export const Headphones = props => {
-  return (
-    <Router>
+let id;
+class Headphones extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: props.items };
+
+    this.handleClick.bind(this);
+  }
+  handleClick(e) {
+    id = e;
+    // console.log("from headphones", id);
+  }
+  render() {
+    return (
+      // <Router>
       <div>
-        <h1 class="category_heading">Headphones</h1>
+        <h1 className="category_heading">Headphones</h1>
         <ul className="categories headphones">
-          {props.items.map(function(item, id) {
+          {this.props.items.map(item => {
             return (
-              <li key={id}>
-                <Link to={`/headphones/${item.brand}-${item.model}`}>
+              <li key={item.id} onClick={e => this.handleClick(item.id)}>
+                <Link to={`/headphone/${item.brand}-${item.model}`}>
                   <div className="headphones--item">
                     <img src={item.url} alt={`item.name`} />
                     <div className="item-desc">
@@ -20,23 +33,84 @@ export const Headphones = props => {
                       <h3 className="item--name">{item.model}</h3>
                       <p className="item--price">{item.price}</p>
                     </div>
-                    {/* <span className="item--desc">{item.desc}</span> */}
                     <button className="btn__addto--cart">Add To Cart</button>
                   </div>
                 </Link>
               </li>
-              // return (
             );
           })}
           <Route
-            path={`/headphones/:headphoneId`}
-            render={() => (
-              <Headphone item={Products.HEADPHONES} key={"headphoneId"} />
-            )}
-            // component={Headphone}
+            path="/headphone/:headphoneId"
+            render={() => <Headphone item={Products.HEADPHONES} id={id} />}
           />
+          {/* <AppRouter id={id} /> */}
         </ul>
       </div>
-    </Router>
-  );
-};
+      // {/* </Router> */}
+    );
+  }
+}
+
+export default Headphones;
+
+// let mapped;
+
+// class Headphones extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       products: [props.items]
+//     };
+//   }
+//   componentDidMount() {
+//     axios.get("/headphones").then(response => {
+//       this.setState(() => {
+//         return {
+//           products: response.data
+//         };
+//       });
+//     });
+//   }
+
+//   render() {
+//     <div>
+//       <h1 className="category_heading">Headphones</h1>
+//       <ul className="categories headphones">
+//         {
+//           (mapped = this.state.products.map(item => {
+//             return (
+//               <Router>
+//                 <li>
+//                   <Link to={`/headphones/${item.brand}-${item.model}`}>
+//                     <div className="headphones--item">
+//                       <img src={item.url} alt={`item.name`} />
+//                       <div className="item-desc">
+//                         <h1 className="item--brand">{item.brand}</h1>
+//                         <h3 className="item--name">{item.model}</h3>
+//                         <p className="item--price">{item.price}</p>
+//                       </div>
+//                       <button className="btn__addto--cart">Add To Cart</button>
+//                     </div>
+//                   </Link>
+//                 </li>
+//                 <Route
+//                   path={`/headphones/:headphoneId`}
+//                   render={() => (
+//                     <Headphone
+//                       brand={item.brand}
+//                       model={item.model}
+//                       price={item.price}
+//                     />
+//                   )}
+//                 />
+//               </Router>
+//             );
+//           }))
+//         }
+//       </ul>
+//     </div>;
+//     return mapped;
+//   }
+// }
+
+// export default Headphones;
