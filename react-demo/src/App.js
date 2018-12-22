db665a2ec7eb2828c939a1bd929a2817ddc56023
jrdleto <datuna_qarseladze@yahoo.com>
2018-12-22 12:16:31 +0400

    slider problem resolved

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
-// import 'antd/dist/antd.css';
+import 'antd/dist/antd.css';
 
 class App extends Component {
   render() {
