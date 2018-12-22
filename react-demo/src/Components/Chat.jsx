003a56ea20789cc15bf4616fef0d3a8d1d1dbb8a
jrdleto <datuna_qarseladze@yahoo.com>
2018-12-22 12:13:14 +0400

    jrdleto1 modified chat

--- react-demo/src/Components/Chat.jsx
+++ react-demo/src/Components/Chat.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import "../css/chat.css";
+
 
 class Chat extends React.Component {
   state = {
@@ -32,7 +34,15 @@ class ChatResult extends React.Component {
   render () {
     return (
       <div className='chat--text'>
-        <h1>Hello</h1>
+       <div class="form-style-6">
+<h1>Contact Us</h1>
+<form>
+<input type="text" name="field1" placeholder="Your Name" />
+<input type="email" name="field2" placeholder="Email Address" />
+<textarea name="field3" placeholder="Type your Message"></textarea>
+<input type="submit" value="Send" />
+</form>
+</div>
       </div>
     )
   }
