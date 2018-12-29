59a8a59b9c8fb6022a42bb173eb7108024dde4cf
sabamamasakhlisi <mamasaxlisi.saba@gmail.com>
2018-12-30 00:58:41 +0400

    search modified

--- react-demo/src/Components/searchedPro.jsx
+++ react-demo/src/Components/searchedPro.jsx
@@ -4,24 +4,40 @@ import {Link} from 'react-router-dom'
 class SearchedPro extends Component {
 
     render(){
+      function convertCat(x){
+        if(x==1){
+          return "cameras"
+        }
+        if(x==2){
+          return "phones"
+        }
+        if(x==3){
+          return "headphones"
+        }
+        if(x==4){
+          return "laptops"
+        }
+      }
       console.log(this.props.info)
         if(this.props.info !=undefined) {
-        
-        return(<form className="searched__item" >{ 
-          this.props.info.map((product)=>
-            <div className="searched__item-cont">
+        console.log(this.props.cat)
+        return(<ul className="items__wrapper" >{ 
+          this.props.info.map((product,index)=>
+            <li className="item" key={index}>
+            <a className="item--div" href={"/" + convertCat(this.props.cat.charAt(index))+'/'+product.id}  >
               <img
                 src={product.url}
-                className="searched__item--img"
+                className="item--img"
                 alt={`${this.props.info.model}`}
               />
-              <h1 className="searched__item--brand">{product.brand}</h1>
-              <h2 className="searched__item--model">{product.model}</h2>
+              <div className="item--spec">
+              <h1 className="item--brand">{product.brand}</h1>
+              <h2 className="item--name">{product.model}</h2>
               {/* <span className="searched__item--desc">{product.desc}</span> */}
-              <span className="searched__item--price">{product.price}</span>
-             <button className="searched__item--addto--cart"> Read More...</button>
-            </div>
-        )}</form>)
+              <p className="item--price">{product.price}</p>
+            </div> <button className="item--btn"> Read More...</button></a>
+            </li>
+        )}</ul>)
 
         }  else return <div></div>
     }
